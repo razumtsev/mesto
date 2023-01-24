5b8ac5be864e25e004a75ad7ca33dc7e5a8ed2a6
Ilya Razumtsev <razumtsev.il@yandex.ru>
2023-01-24 14:10:37 +0300

    cards from box and likes func added

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,30 @@ const formEditProfile = document.querySelector('.form_type_edit-profile');
 const inputName = formEditProfile.querySelector('.form__input_type_name');
 const inputDescription = formEditProfile.querySelector('.form__input_type_description');
 const buttonClosePopupList = document.querySelectorAll('.button_type_close');
+const elementsGrid = document.querySelector('.elements__grid');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const addCardPrepend = card => elementsGrid.prepend(card);
+
+const handleLikeClick = evt => evt.target.classList.toggle('card__like_is-active');
+
+const createCard = item => {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const link = card.querySelector('.card__image');
+  const name = card.querySelector('.card__caption');
+  const like = card.querySelector('.card__like')
+  link.src = item.link;
+  link.alt = item.name;
+  name.textContent = item.name;
+  like.addEventListener('click', handleLikeClick);
+  return card;
+}
+
+initialCards.forEach(item => addCardPrepend(createCard(item)));
+
+
+
+
 
 function closePopup() {
   popupList.forEach(function(item) {
@@ -15,7 +39,7 @@ function closePopup() {
   })
 }
 
-function openPopup(popupName) {
+const openPopup = (popupName) => {
   popupName.classList.add('popup_is-open');
 }
 
