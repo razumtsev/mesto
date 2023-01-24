86d23cdc21e61f7867011cafd2861a4bcb78a6cd
Ilya Razumtsev <razumtsev.il@yandex.ru>
2023-01-24 17:08:10 +0300

    code refactoring is done

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,26 @@
-// очень глобальные переменные
-const profile = document.querySelector('.profile');
+// очень-очень глобальные переменные
+const page = document.querySelector('.page');
+const profile = page.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileDescription = profile.querySelector('.profile__description');
-// модальное окно редактирования профиля
+// глобальные переменные для модального окна редактирования профиля
 const buttonEditProfile = profile.querySelector('.button_type_edit-profile');
-const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const formEditProfile = document.querySelector('.form_type_edit-profile');
+const popupEditProfile = page.querySelector('.popup_type_edit-profile');
+const formEditProfile = page.querySelector('.form_type_edit-profile');
 const inputName = formEditProfile.querySelector('.form__input_type_name');
 const inputDescription = formEditProfile.querySelector('.form__input_type_description');
-// модальное окно добавления карточки
+// глобальные переменные для модального окна добавления карточки
 const buttonAddCard = profile.querySelector('.button_type_add-card');
-const popupAddCard = document.querySelector('.popup_type_add-card');
-const formAddCard = document.querySelector('.form_type_add-card');
+const popupAddCard = page.querySelector('.popup_type_add-card');
+const formAddCard = page.querySelector('.form_type_add-card');
 const inputCardName = formAddCard.querySelector('.form__input_type_card-name');
 const inputCardLink = formAddCard.querySelector('.form__input_type_card-link');
-// закрытие попапов
-const popupList = document.querySelectorAll('.popup');
-const buttonClosePopupList = document.querySelectorAll('.button_type_close');
-// темплейт карточки
-const elementsGrid = document.querySelector('.elements__grid');
-const cardTemplate = document.querySelector('#card-template').content;
+// глобальные переменные для закрытия модальных окон
+const popupList = page.querySelectorAll('.popup');
+const buttonClosePopupList = page.querySelectorAll('.button_type_close');
+// глобальные переменные темплейта карточки
+const elementsGrid = page.querySelector('.elements__grid');
+const cardTemplate = page.querySelector('#card-template').content;
 
 
 // создание и добавление карточки на страницу
@@ -44,41 +45,50 @@ const createCard = item => {
 initialCards.forEach(item => addCardAppend(createCard(item)));
 
 
-
-// прочее неструктурированное
-
 // закрытие модальных окон
+
 const closePopup = () => popupList.forEach(item => item.classList.remove('popup_is-open'));
 
 buttonClosePopupList.forEach(button => button.addEventListener('click', closePopup));
 
 // открытие модальных окон
+
 const openPopup = (popupName) => {
   popupName.classList.add('popup_is-open');
 }
 
 // обработчики событий
 
-buttonEditProfile.addEventListener('click', () => {
+const handleEditProfileButton = () => {
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
   openPopup(popupEditProfile);
-});
+}
 
-formEditProfile.addEventListener('submit', evt => {
+const handleEditProfileSubmit = evt => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
   closePopup();
-});
+}
 
-buttonAddCard.addEventListener('click', () => openPopup(popupAddCard));
+const handleAddCardButton = () => {
+  openPopup(popupAddCard)
+}
 
-formAddCard.addEventListener('submit', evt => {
+const handleAddCardSubmit = evt => {
   evt.preventDefault();
   const cardEssence = {};
   cardEssence.name = inputCardName.value;
   cardEssence.link = inputCardLink.value;
   addCardPrepend(createCard(cardEssence));
   closePopup();
-});
+  formAddCard.reset();
+}
+
+// слушатели событий
+
+buttonEditProfile.addEventListener('click', handleEditProfileButton);
+formEditProfile.addEventListener('submit', handleEditProfileSubmit);
+buttonAddCard.addEventListener('click', handleAddCardButton);
+formAddCard.addEventListener('submit', handleAddCardSubmit);
